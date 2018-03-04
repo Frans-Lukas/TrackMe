f259dea3d27d0632b682edeef078c041864b27a9
Frans-Lukas <franslukas@gmail.com>
2018-03-04 13:25:45 +0100

    Added permission asking and map zooming to your location.

--- app/src/main/java/c16fld/cs/umu/se/trackme/MapsActivity.java
+++ app/src/main/java/c16fld/cs/umu/se/trackme/MapsActivity.java
@@ -1,46 +1,91 @@
 package c16fld.cs.umu.se.trackme;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng mMyLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        checkFineLocationPermission();
+
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new MyOnSuccessListener());
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private void checkFineLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in Sydney, Australia, and move the camera.
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private class MyOnSuccessListener implements OnSuccessListener<Location>{
+        @Override
+        public void onSuccess(Location location) {
+            if(location != null){
+                mMyLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mMyLocation, 15));
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION:{
+                if(grantResults.length > 0
+                   && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    checkFineLocationPermission();
+                    mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new MyOnSuccessListener());
+                    if(mMyLocation != null){
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(mMyLocation));
+                    }
+                }
+            }
+        }
     }
 }
