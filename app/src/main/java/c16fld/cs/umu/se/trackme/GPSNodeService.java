30bece6c88d59f1e0a5692811c6fcdf733968e70
Frans-Lukas <franslukas@gmail.com>
2018-03-05 17:40:06 +0100

    Added attributes to service class.

--- app/src/main/java/c16fld/cs/umu/se/trackme/GPSNodeService.java
+++ app/src/main/java/c16fld/cs/umu/se/trackme/GPSNodeService.java
@@ -3,16 +3,36 @@ package c16fld.cs.umu.se.trackme;
 import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by Frans-Lukas on 2018-03-05.
  */
 
 public class GPSNodeService extends Service {
-    boolean hasPermissionToTrack = false;
+    public final static int MINUTE = 1000 * 60;
+
+    private boolean isGPSEnabled = false;
+    private boolean isNetworkEnabled = false;
+    private boolean canGetLocation = false;
+    private boolean hasPermissionToTrack = false;
+
+    private Location lastLocation;
+
+    private static final long MIN_DISTANCE_FOR_UPDATE = 10;
+
+    private int TIME_BETWEEN_UPDATES = 1 * MINUTE;
+
+    private LocationManager locationManager;
+
+
+    
 
     @Override
     public void onCreate() {
@@ -26,7 +46,7 @@ public class GPSNodeService extends Service {
                     "Does not have permission to track. Stopping.",
                     Toast.LENGTH_SHORT).
                     show();
-            
+
             stopSelf();
         }
         return super.onStartCommand(intent, flags, startId);
