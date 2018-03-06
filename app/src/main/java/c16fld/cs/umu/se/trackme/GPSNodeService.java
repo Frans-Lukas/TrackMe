0a4fad1ca28b8380ffc62c13c039dcbeb09a9b09
Frans-Lukas <franslukas@gmail.com>
2018-03-06 17:19:55 +0100

    Added location service class that will add nodes to the database.

--- app/src/main/java/c16fld/cs/umu/se/trackme/GPSNodeService.java
+++ app/src/main/java/c16fld/cs/umu/se/trackme/GPSNodeService.java
@@ -1,42 +1,64 @@
 package c16fld.cs.umu.se.trackme;
 
+import android.Manifest;
 import android.app.IntentService;
 import android.app.Service;
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
+import android.arch.persistence.room.Room;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 /**
  * Created by Frans-Lukas on 2018-03-05.
  */
 
 public class GPSNodeService extends Service {
-    public final static int MINUTE = 1000 * 60;
-
-    private boolean isGPSEnabled = false;
-    private boolean isNetworkEnabled = false;
-    private boolean canGetLocation = false;
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
     private boolean hasPermissionToTrack = false;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private int lastUsedID = 0;
 
-    private Location lastLocation;
-
-    private static final long MIN_DISTANCE_FOR_UPDATE = 10;
-
-    private int TIME_BETWEEN_UPDATES = 1 * MINUTE;
-
-    private LocationManager locationManager;
-
-
-    
+    private NodeDB mDataBase;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        mDataBase = Room.databaseBuilder(
+                getApplicationContext(),
+                NodeDB.class,
+                "Node-database")
+                .build();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    public void getLocation(){
+    }
+
+    class MySuccessListener implements OnSuccessListener<Location>{
+
+
+        @Override
+        public void onSuccess(Location location) {
+            NodeEntity node = new NodeEntity(
+                    lastUsedID + 1,
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    lastUsedID);
+            mDataBase.nodeDao().insertAll(node);
+        }
     }
 
     @Override
