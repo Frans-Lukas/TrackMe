5b6b1cf3e4ad5d4032b6987af1c79ab6203ce619
Frans-Lukas <franslukas@gmail.com>
2018-03-07 16:16:59 +0100

    Implemented gps service class

--- app/src/main/java/c16fld/cs/umu/se/trackme/GPSNodeService.java
+++ app/src/main/java/c16fld/cs/umu/se/trackme/GPSNodeService.java
@@ -3,44 +3,114 @@ package c16fld.cs.umu.se.trackme;
 import android.Manifest;
 import android.app.Service;
 import android.arch.persistence.room.Room;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Frans-Lukas on 2018-03-05.
  */
 
-public class GPSNodeService extends Service {
-    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
-    private boolean hasPermissionToTrack = false;
-    private FusedLocationProviderClient mFusedLocationClient;
+public class GPSNodeService extends Service  {
+    private int mSecond = 1000;
+    private int mMinute = mSecond * 60;
+    private int mHalfHour = mMinute * 30;
+    private int mMinDistance = 200;
     private int lastUsedID = 0;
 
+    private LocationManager mLocationManager;
+    LocationListener[] mLocationListeners;
     private NodeDB mDataBase;
+
+    private class MyLocationListener implements LocationListener{
+
+        @Override
+        public void onLocationChanged(Location location) {
+            NodeEntity node = new NodeEntity(
+                    lastUsedID + 1,
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    lastUsedID);
+            mDataBase.nodeDao().insertAll(node);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+            Log.d(TAG, "onStatusChanged: " + s);
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+            Log.d(TAG, "onProviderEnabled: " + s);
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            Log.d(TAG, "onProviderDisabled: " + s);
+        }
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.e(TAG, "onCreate GPSService");
         mDataBase = Room.databaseBuilder(
                 getApplicationContext(),
                 NodeDB.class,
                 "Node-database")
                 .build();
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationManager = (LocationManager)
+                getApplicationContext().
+                getSystemService(Context.LOCATION_SERVICE);
+
+        mLocationListeners = new LocationListener[]{
+                new MyLocationListener()
+        };
+
+        updateLastUsedDBID();
+
     }
 
-    public void getLocation() {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
         checkFineLocationPermission();
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(new MySuccessListener());
+        mLocationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                mHalfHour,
+                mMinDistance,
+                mLocationListeners[0]);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void updateLastUsedDBID() {
+        ArrayList<NodeEntity> nodeEntities = (ArrayList<NodeEntity>) mDataBase.nodeDao().getAll();
+
+        boolean foundNewId = false;
+        while(!foundNewId){
+
+            foundNewId = true;
+            for (NodeEntity nodeEntity : nodeEntities) {
+                if(nodeEntity.getId() == lastUsedID){
+                    lastUsedID++;
+                    foundNewId = false;
+                }
+            }
+
+        }
     }
 
     private void checkFineLocationPermission() {
@@ -51,25 +121,6 @@ public class GPSNodeService extends Service {
                     show();
             stopSelf();
         }
-    }
-
-    class MySuccessListener implements OnSuccessListener<Location>{
-        @Override
-        public void onSuccess(Location location) {
-            if(location != null) {
-                NodeEntity node = new NodeEntity(
-                        lastUsedID + 1,
-                        location.getLatitude(),
-                        location.getLongitude(),
-                        lastUsedID);
-                mDataBase.nodeDao().insertAll(node);
-            }
-        }
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
--- app/src/main/java/c16fld/cs/umu/se/trackme/MapsActivity.java
+++ app/src/main/java/c16fld/cs/umu/se/trackme/MapsActivity.java
@@ -3,6 +3,7 @@ package c16fld.cs.umu.se.trackme;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -10,6 +11,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationRequest mLocationRequest;
+
 
 
     @Override
@@ -21,6 +24,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        checkLocationSettings();
+        setViewToLastKnownLocation();
+    }
+
+    private void setViewToLastKnownLocation() {
+    }
+
+    private void checkLocationSettings() {
+
     }
 
     @Override
