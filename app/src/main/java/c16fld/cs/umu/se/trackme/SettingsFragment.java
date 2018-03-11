7dda3bb160d57dbe889d0688793617b82ba75269
Frans-Lukas <franslukas@gmail.com>
2018-03-11 14:48:15 +0100

    Added settings fragment.

--- app/src/main/java/c16fld/cs/umu/se/trackme/SettingsFragment.java
+++ app/src/main/java/c16fld/cs/umu/se/trackme/SettingsFragment.java
@@ -1,16 +1,17 @@
 package c16fld.cs.umu.se.trackme;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
-public class SettingsActivity extends Activity {
+/**
+ * Created by Frans-Lukas on 2018-03-11.
+ */
 
+public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
